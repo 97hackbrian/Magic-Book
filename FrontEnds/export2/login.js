ac39bf9a70d45c2a52c3c938351cccb53eaffdef
97hackbrian <brayandurantoconas@gmail.com>
2023-01-16 16:16:17 -0400

    LOginJsV2

--- FrontEnds/export2/login.js
+++ FrontEnds/export2/login.js
@@ -2,12 +2,14 @@ var attempt=4;
 function validate(){
     var password=document.getElementById("exampleInputPassword").value;
     if(password=="admin"){
-        alert("ingreso correcto");
-        window.location="4Balance.html";
+        alert("ingreso correcto  + ++ ss");
+        window.open("4Balance.html","fullscreen", 'top=0,left=0,width='+(screen.availWidth)+',height ='+(screen.availHeight)+',fullscreen=yes,toolbar=0 ,location=0,directories=0,status=0,menubar=0,resiz able=0,scrolling=0,scrollbars=0');
+        return false;
     }
     else{
         attempt--;
         alert("contraseña incorrecta");
+        return false;
     }
 }
 function empleado(){
