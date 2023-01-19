e59f7ceb4b3c1e8c16fec61ed0a98651363c9a78
97hackbrian <brayandurantoconas@gmail.com>
2023-01-19 17:41:01 -0400

    DataBaseVf1

--- FrontEnds/export4/database.js
+++ FrontEnds/export4/database.js
@@ -1,5 +1,7 @@
-var indexedDB=window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 var dataBase=null;
+let timeout;
+let ide;
 function startDB(){
     dataBase = indexedDB.open("object",1);
     //indexedDB.createTable("djs");
@@ -17,40 +19,81 @@ function startDB(){
         object.createIndex('precio','dni',{unique:false});
     }
     dataBase.onsuccess=function(e){
-        alert("database loaded");
+
         loadAll();
+        //count();
+        
+setTimeout(() => {
+  console.log("Base de datos correctamente leida")
+}, 2000);
     }
 
 }
 
 function add(){
+//	count();
     var active =dataBase.result;
     var data = active.transaction(["items"],"readwrite");
     var object = data.objectStore("items")
     if(document.getElementById("ingresarCode").value=="" || document.getElementById("ingresarname").value=="" || document.getElementById("ingresarprecio").value==""){
-    alert("¡ingrese todos los campos por favor!");
+    alert("¡ingrese todos los campos para añadir por favor!");
     }
     else{
+    data.oncomplete=function(e){
+//    	loadAll();
+        alert("Guardado exitosamente el Item : \n"+" Código: "+document.getElementById("ingresarCode").value+" \n Nombre: "+document.getElementById("ingresarname").value+" \n Precio: "+document.getElementById("ingresarprecio").value);
+        loadAll();
+    }
     var request = object.put({
         code: document.getElementById("ingresarCode").value,
         nombre: document.getElementById("ingresarname").value,
         precio: document.getElementById("ingresarprecio").value
     });
+    
+        
     }
-    request.onerror=function(e){
-        alert(request.error.name+ '\n\n'+request.error.message);
+    data.onerror=function(e){
+        alert("error"+request.error.name+ '\n\n'+request.error.message);
     }
-    data.oncomplete=function(e){
-        alert("guardado exitosamente "+document.getElementById("ingresarCode").value+"  "+document.getElementById("ingresarname").value+"  "+document.getElementById("ingresarprecio").value);
-        loadAll();
-    }
+    
+
+    
 }
 
 function remove(){
+//count();
+		var active = dataBase.result;
+                var data = active.transaction(["items"], "readwrite");
+                var object = data.objectStore("items");
+                
+                let dataTask = document.getElementById("eliminado").value;
+                if(dataTask=="")
+                {
+                	alert("¡ingrese todos los campos para eliminar por favor!");
+                }
+                else{
+//                var index = target.getAttribute('id');;
+                
+//                var index = object.index('code');
+                var request = object.delete(+document.getElementById("eliminado").value);
+                request.onerror = function (e) {
+                    alert(request.error.name + '\n\n' + request.error.message);
+                };
+ 
+                data.oncomplete = function (e) {
+ 			document.getElementById("eliminado").value = '';
+                        document.getElementById("ingresarCode").value = '';
+			document.getElementById("ingresarname").value = '';
+			document.getElementById("ingresarprecio").value = '';
+ 
+                    alert('ITEM '+dataTask+" eliminado correctamente");
+                    loadAll();
+                };
 
 }
 
-function loadAll(){
+}
+function count(){
     var active =dataBase.result;
     var data = active.transaction(["items"],"readonly");
     var object = data.objectStore("items")
@@ -68,12 +111,61 @@ function loadAll(){
     data.oncomplete=function(){
         var outerHMTL='';
         for(var key in elements){
+        	ide=elements[key].id+1;
+        	console.log(ide);
+        }
+        
+        elements.reverse()
+        for(var key in elements){
             outerHMTL+='\n\
             <tr>\n\
+            	<td>'+elements[key].id + '</td>\n\
                 <td>'+elements[key].code + '</td>\n\
                 <td>'+elements[key].nombre + '</td>\n\
                 <td>'+elements[key].precio + '</td>\n\
                 </tr>';
+          
+        }
+        elements=[];
+        document.querySelector("#dataTable2").innerHTML=outerHMTL;       
+    }
+}
+
+
+function loadAll(){
+	document.getElementById("ingresarCode").value = '';
+	document.getElementById("ingresarname").value = '';
+	document.getElementById("ingresarprecio").value = '';
+    var active =dataBase.result;
+    var data = active.transaction(["items"],"readonly");
+    var object = data.objectStore("items")
+    var elements=[];
+
+    object.openCursor().onsuccess=function(e){
+        var result =e.target.result;
+        if(result==null){
+            return;
+        }
+        
+        elements.push(result.value);
+        result.continue();
+    };
+    data.oncomplete=function(){
+        var outerHMTL='';
+        for(var key in elements){
+        //	ide=elements[key].id+2;
+        }
+        
+        elements.reverse()
+        for(var key in elements){
+            outerHMTL+='\n\
+            <tr>\n\
+            	<td>'+elements[key].id + '</td>\n\
+                <td>'+elements[key].code + '</td>\n\
+                <td>'+elements[key].nombre + '</td>\n\
+                <td>'+elements[key].precio + '</td>\n\
+                </tr>';
+          
         }
         elements=[];
         document.querySelector("#dataTable2").innerHTML=outerHMTL;       
