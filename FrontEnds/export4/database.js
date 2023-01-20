236b4e01a8c98c944eb1f908c3bc051c52c348d0
97hackbrian <brayandurantoconas@gmail.com>
2023-01-20 00:43:53 -0400

    DataF

--- FrontEnds/export4/database.js
+++ FrontEnds/export4/database.js
@@ -1,7 +1,8 @@
 var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 var dataBase=null;
-let timeout;
-let ide;
+var timeout;
+var ide;
+
 function startDB(){
     dataBase = indexedDB.open("object",1);
     //indexedDB.createTable("djs");
@@ -136,6 +137,68 @@ function loadAll(){
 	document.getElementById("ingresarCode").value = '';
 	document.getElementById("ingresarname").value = '';
 	document.getElementById("ingresarprecio").value = '';
+    var active =dataBase.result;
+    var data = active.transaction(["items"],"readonly");
+    var object = data.objectStore("items")
+    var elements=[];
+
+    object.openCursor().onsuccess=function(e){
+        var result =e.target.result;
+        if(result==null){
+            return;
+        }
+        
+        elements.push(result.value);
+        result.continue();
+    };
+    data.oncomplete=function(){
+        var outerHMTL='';
+        for(var key in elements){
+        //	ide=elements[key].id+2;
+        }
+        
+        elements.reverse()
+        for(var key in elements){
+            outerHMTL+='\n\
+            <tr>\n\
+            	<td>'+elements[key].id + '</td>\n\
+                <td>'+elements[key].code + '</td>\n\
+                <td>'+elements[key].nombre + '</td>\n\
+                <td>'+elements[key].precio + '</td>\n\
+                </tr>';
+          
+        }
+        elements=[];
+        document.querySelector("#dataTable2").innerHTML=outerHMTL;       
+    }
+}
+
+function loadAll2(){
+
+    dataBase = indexedDB.open("object",1);
+    //indexedDB.createTable("djs");
+    dataBase.onupgradeneeded=function(e){
+        alert("creado");
+        var active =dataBase.result;
+        var Option ={
+            keyPath:'id',
+            autoIncrement: true
+        };
+
+        var object = active.createObjectStore("items",Option);
+        object.createIndex('code','dni',{unique:false});
+        object.createIndex('nombre','name',{unique:false});
+        object.createIndex('precio','dni',{unique:false});
+    }
+    dataBase.onsuccess=function(e){
+
+
+  console.log("Base de datos correctamente leida")
+
+    }
+
+
+
     var active =dataBase.result;
     var data = active.transaction(["items"],"readonly");
     var object = data.objectStore("items")
