ef180f44838a33a56d87bb5d8c84137885191a34
97hackbrian <brayandurantoconas@gmail.com>
2023-01-23 02:11:03 -0400

    f5VenderBalance

--- FrontEnds/export4/database.js
+++ FrontEnds/export4/database.js
@@ -457,6 +457,7 @@ vec1();
 
 
 function Vender(){
+    addC();
     alert("Vendido! "+suma+"Bs.");
 suma=0;
 var req = indexedDB.deleteDatabase("object2");
@@ -478,6 +479,7 @@ req.onerror = function () {
 
 let dataBase2;
 function startDB2(){
+    startDBC();
     dataBase2 = indexedDB.open("object",1);
 	//console.log(<?php echo $variable1; ?>);
     dataBase = indexedDB.open("object2",1);
@@ -508,3 +510,58 @@ setTimeout(() => {
 }
 
 
+var indexedDB2 = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+var dataBaseC=null;
+
+function startDBC(){
+    dataBaseC = indexedDB2.open("Reporte",1);
+    //indexedDB.createTable("djs");
+    dataBaseC.onupgradeneeded=function(e){
+        alert("creado");
+        var active =dataBaseC.result;
+        var Option ={
+            keyPath:'id',
+            autoIncrement: true
+        };
+
+        var object = active.createObjectStore("items",Option);
+        object.createIndex('diario','d',{unique:false});
+        object.createIndex('semanal','s',{unique:false});
+        object.createIndex('mensuales','m',{unique:false});
+    }
+    dataBaseC.onsuccess=function(e){
+
+        //loadC();
+        //count();
+        
+setTimeout(() => {
+  console.log("Base de datos correctamente leida")
+}, 2000);
+    }
+
+}
+
+function addC(){
+    //	count();
+        var active =dataBaseC.result;
+        var data = active.transaction(["items"],"readwrite");
+        var object = data.objectStore("items")
+ 
+ 
+        data.oncomplete=function(e){
+    //    	loadAll();
+            alert("Guardado exitosamente el pago");
+            //loadAll();
+        }
+        var request = object.put({
+            day: suma
+        });
+        
+            
+        
+        data.onerror=function(e){
+            alert("error"+request.error.name+ '\n\n'+request.error.message);
+        }
+        
+    }
+    
