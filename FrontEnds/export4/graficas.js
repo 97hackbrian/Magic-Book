0dd4963d2d7def9695c17eb2ac9b2eeea4c1b5d3
97hackbrian <brayandurantoconas@gmail.com>
2023-02-02 00:10:17 -0400

    grafic_testV1

--- FrontEnds/export4/graficas.js
+++ FrontEnds/export4/graficas.js
@@ -1,5 +1,42 @@
-let miCanvas=document.getElementById("Gsemana").getContext("2d");
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 
+//let miCanvas=document.getElementById("Gsemana").getContext("2d");
+var vectorX2=new Array;
+
+function Lervec(){
+var dataBaseC=indexedDB2.open("date",1);
+
+        var active =dataBaseC.result;
+	var active =dataBaseC.result;
+	var data = active.transaction(["items"],"readonly");
+	var object = data.objectStore("items")
+	var elements=[];
+	var dia=0;
+
+	object.openCursor().onsuccess=function(e){
+		var result =e.target.result;
+		if(result==null){
+			return;
+		}
+		
+		vectorX2.push(result.value);
+		
+//        vector.push(elements);
+	vector.push(result.value);
+		
+		result.continue();
+	};
+	data.oncomplete=function(){
+		//var outerHMTL='';
+		for(var key in elements){
+		//	ide=elements[key].id+2;
+			dia=parseInt(elements[key].day)+parseInt(dia);
+
+		}
+
+    }
+}
+/*
 var chart=new Chart(miCanvas,{
     type:"bar",
     data:{
@@ -14,4 +51,4 @@ var chart=new Chart(miCanvas,{
             }
         ]
     }
-});
+});*/
